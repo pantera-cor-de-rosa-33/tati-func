f08b61998a398fdad33ce289060c776bf5fbec9d
pantera-cor-de-rosa-33 <tatiane.corsi@aluno.imepac.edu.br>
2025-02-27 21:54:52 -0300

    comitando metodo 03

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,4 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Tati");
     }
+
+    public static int fatorial(int num  ){
+        int result =1;
+        for(int i = 1;i <= num;i++){
+            result *= i;
+        }
+        return result;
+    }
+
 }
