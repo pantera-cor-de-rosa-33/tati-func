bb29f0f0f708376941a896fd16f0e936023ba868
pantera-cor-de-rosa-33 <tatiane.corsi@aluno.imepac.edu.br>
2025-02-27 21:16:39 -0300

    Adicionei o metodo de soma

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Tati");
+        int s = soma(56, 67);
+        System.out.println("Soma de 56 + 67 = " + s);
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
     }
 }
