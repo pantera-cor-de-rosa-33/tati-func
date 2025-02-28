584607c37a49551ea642a9c72d4f579db0bb3268
pantera-cor-de-rosa-33 <tatiane.corsi@aluno.imepac.edu.br>
2025-02-27 21:39:08 -0300

    segundo commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,6 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Tati");
+
     }
+
+    public static boolean ePar(int num){
+        if (num % 2 == 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
