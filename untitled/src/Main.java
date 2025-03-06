ed3edb099e61a23a9d5aca509e5ee3a487e5d3b6
pantera-cor-de-rosa-33 <tatiane.corsi@aluno.imepac.edu.br>
2025-03-06 20:03:14 -0300

    Metodo media

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Tati");
     }
+    public static int medi(int num1,int num2,int num3){
+        int result = (num1+ num2 + num3 ) / 3;
+        return result;
+
+    }
 }
