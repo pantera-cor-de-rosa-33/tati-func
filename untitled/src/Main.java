490bb0a98204d6dcbfffc77bf318024bc24cc489
pantera-cor-de-rosa-33 <tatiane.corsi@aluno.imepac.edu.br>
2025-03-06 20:23:31 -0300

    Metodo contador de vogal

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,4 +4,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Tati");
     }
+    public static int contarVogais (String str) {
+        int contador = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+
+            }
+        }
+        return contador;
+    }
 }
